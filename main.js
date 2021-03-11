fe9af31d23788e52b64e04e05007c28f05a2a601
n-desantis92 <dsnwebdev@gmail.com>
2021-03-11 17:25:42 +0100

    aggiunto jguery con addClass e removeClass

--- main.js
+++ main.js
@@ -2,16 +2,32 @@ var mediaMenu = $(".header-right > a");
 var openMenu = $(".hamburger-menu");
 var closeMenu = $(".close");
 
-// apertura menu al click
+// // apertura menu al click
+// mediaMenu.click(
+//   function () {
+//     openMenu.show(500);
+//   }
+// );
+
+// // chiusura menu al click
+// closeMenu.click(
+//   function () {
+//     openMenu.hide(500);
+//   }
+// );
+
+
+
+// // apertura menu al click con aggiunta classe
 mediaMenu.click(
   function () {
-    openMenu.show(500);
+    openMenu.addClass("active");
   }
 );
 
 // chiusura menu al click
 closeMenu.click(
   function () {
-    openMenu.hide(500);
+    openMenu.removeClass("active");
   }
 );
